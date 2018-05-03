ee869c81a692b1335eef20e9b3c1c20f2e0f2cfd
erosyip <33865728+erosyip@users.noreply.github.com>
2018-05-03 11:43:33 +0800

    Add files via upload

---
+++ WelcomeVC.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by YIP CHAK HO EROS on 21/4/2018.
+//  Copyright © 2018 YIP CHAK HO EROS. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
